d7f7969b43358de81fcd0b4989b3f022a249044b
ros2 <guru.m@otworld.in>
2025-04-04 19:58:01 +0530

    Updated proto directory

--- generate.sh
+++ generate.sh
@@ -29,19 +29,19 @@ echo "Processing $FLEET_MANAGER_PROTO file.."
 
 # Compiles Python files for the agent.proto file
 ##### NOTE: Change the local GT-Agent repo directory to save the generated Python pb files ####
-python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/RCC/robotix-agent/node --grpc_python_out=/home/amizhthan/RCC/robotix-agent/node ./protos/agent/v1/agent.proto
+python3 -m grpc_tools.protoc -I. --python_out=/home/ros2/robotix-agent/node --grpc_python_out=/home/ros2/robotix-agent/node ./protos/agent/v1/agent.proto
 
 # Loops over all the model files and creates both Go and Python files
 for f in ./protos/model/v1/*.proto
 do
     protoc --go_out=. --go-grpc_out=. $f
-    python3 -m grpc_tools.protoc -I. --python_out=/home/amizhthan/RCC/robotix-agent/node --grpc_python_out=/home/amizhthan/RCC/robotix-agent/node $f
+    python3 -m grpc_tools.protoc -I. --python_out=/home/ros2/robotix-agent/node --grpc_python_out=/home/ros2/robotix-agent/node $f
 
     echo "Processing $f file.."
 done
 
 # Move generated Go files to ./protos/v1/
-mv ./github.com/GOAT-Robotics/gtstudio-proto/protos/v1/* ./protos/v1/
+mv ./github.com/GOAT-Robotics/robotix-proto/protos/v1/* ./protos/v1/
 
 # remove the github.com directory
 rm -rf ./github.com
