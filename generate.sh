c93aa1a1534f69aeaa8fbd4e085c0b02601b7ac2
cipher <anandharaj@goat-robotics.com>
2025-09-05 16:26:58 +0530

    Updated protos

--- generate.sh
+++ generate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 RCC_PROTO="./protos/rcc/v1/rcc.proto"
-NODE_SDK="./protos/node_sdk/v1/node_sdk.proto"
-AGENT_PROTO="./protos/agent/v1/agent.proto"
 FLEET_MANAGER_PROTO="./protos/fleet_manager/v1/fleet_manager.proto"
 
 # Remove existing Go files
@@ -15,21 +13,13 @@ mkdir ./protos/v1
 protoc --go_out=. --go-grpc_out=. $RCC_PROTO
 echo "Processing $RCC_PROTO file.."
 
-# Generates Go files for GT Studio - Node SDK from the node_sdk.proto file
-protoc --go_out=. --go-grpc_out=. $NODE_SDK
-echo "Processing $NODE_SDK file.."
+# Node SDK generation removed (deprecated)
 
-# Generates Go files for GT Studio - Agent from the agent.proto file
-protoc --go_out=. --go-grpc_out=. $AGENT_PROTO
-echo "Processing $AGENT_PROTO file.."
+# Generates Go files for GT Studio - Agent from the agent.proto file (deprecated)
 
 # Generates Go files for GT Studio - Fleet Manager from the fleet_manager.proto file
 protoc --go_out=. --go-grpc_out=. $FLEET_MANAGER_PROTO
 echo "Processing $FLEET_MANAGER_PROTO file.."
-
-# Compiles Python files for the agent.proto file
-##### NOTE: Change the local GT-Agent repo directory to save the generated Python pb files ####
-# python3 -m grpc_tools.protoc -I. --python_out=$HOME/robotix-agent/node --grpc_python_out=$HOME/robotix-agent/node ./protos/agent/v1/agent.proto
 
 # Loops over all the model files and creates both Go and Python files
 for f in ./protos/model/v1/*.proto
